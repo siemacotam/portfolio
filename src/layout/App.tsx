a8adcfebd9240a5ada2a3916d7ceb3ecc33e391b
Grzegorz Celek <g.celek@gmail.com>
2022-02-13 23:01:40 +0100

    .

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
 import Main from './Main'
+import '../sass/style.sass'
 
 const App = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false)
